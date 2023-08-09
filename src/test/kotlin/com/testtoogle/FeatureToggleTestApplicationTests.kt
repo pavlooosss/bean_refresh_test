ab1950eb185a95154bec8e246575ee92e9c78ab1
Anton Duyun <a.duyun@tinkoff.ru>
2023-08-09 12:57:59 +0300

    test

--- src/test/kotlin/com/testtoogle/FeatureToggleTestApplicationTests.kt
+++ src/test/kotlin/com/testtoogle/FeatureToggleTestApplicationTests.kt
@@ -1,13 +1,32 @@
 package com.testtoogle
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.context.scope.refresh.RefreshScope
 
 @SpringBootTest
 class FeatureToggleTestApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    lateinit var refresh: RefreshScope
+
+    @Autowired
+    lateinit var ps: TestPropertySource
+
+    @Autowired
+    @Qualifier("interface")
+    lateinit var service: PerfectService
+
+    @Test
+    fun contextLoads() {
+        assert(service.getStringValue() == "First Implementation")
+
+        ps.value = "SECOND"
+        refresh.refreshAll()
+
+        assert(service.getStringValue() == "Second Implementation")
+    }
 
 }
