3cb3844d0fda66f66dede4b6a985f60d3fd5ff13
Anton Duyun <a.duyun@tinkoff.ru>
2023-08-09 12:43:21 +0300

    refreshable bean example

--- src/main/kotlin/com/testtoogle/FirstImpl.kt
+++ src/main/kotlin/com/testtoogle/FirstImpl.kt
@@ -1,19 +1,7 @@
 package com.testtoogle
 
-import jakarta.annotation.PostConstruct
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.cloud.context.config.annotation.RefreshScope
-import org.springframework.context.annotation.Scope
-import org.springframework.stereotype.Component
-
-//@Component
-//@RefreshScope
-////@ConditionalOnProperty(value = ["feature.toogle.testValue"], havingValue = "FIRST")
-//@Scope("request")
-class FirstImpl: PerfectService {
-
-    @PostConstruct
-    fun postConstruct() {
+class FirstImpl : PerfectService {
+    init {
         println("FIRST BEAN CREATED")
     }
 
